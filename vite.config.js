7e1c61835d7bf9b762199720fa4e48edb03b1d5f
Deepika Venkatesulu <deepikavenkatesulu@Deepikas-MacBook-Pro.local>
2025-01-12 19:38:30 -0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()], build: {
     outDir: 'dist',  // By default, Vite builds to the 'dist' folder
   },
-  base: '/financialapp/'
+  base: '/financial-app/'
 })
