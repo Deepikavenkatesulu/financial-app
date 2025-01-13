8fb8538f43515b3fc0c8d740ba338eda4596bf9a
Deepika Venkatesulu <deepikavenkatesulu@Deepikas-MacBook-Pro.local>
2025-01-12 18:43:42 -0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], build: {
+    outDir: 'dist',  // By default, Vite builds to the 'dist' folder
+  },
+  base: '/financialapp/'
 })
